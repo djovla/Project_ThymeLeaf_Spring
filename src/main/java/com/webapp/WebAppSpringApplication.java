908d545e3351260fbb8933e8f58d4ac1b0417ac2
djovla <laferrierevirgo@gmail>
2022-09-10 21:07:35 -0400

    Modification/pom/tomcat main class to allow war to run in tomcat

--- src/main/java/com/webapp/WebAppSpringApplication.java
+++ src/main/java/com/webapp/WebAppSpringApplication.java
@@ -2,12 +2,18 @@ package com.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebAppSpringApplication {
+public class WebAppSpringApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebAppSpringApplication.class, args);
 	}
-
+	@Override
+    protected SpringApplicationBuilder configure(
+      SpringApplicationBuilder builder) {
+        return builder.sources(WebAppSpringApplication.class);
+    }
 }
